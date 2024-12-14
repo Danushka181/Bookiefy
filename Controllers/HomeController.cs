583a83fac975df35e9bd0ca5b7acddc3d964c3a8
Danushka <danushka181@gmail.com>
2024-12-14 10:30:51 +0530

    site init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        // Constructor
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Load the home page view
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Load the privacy page view
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        // Load the error page view
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
